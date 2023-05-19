154ca118866570f0ce4723a96a7ccd3503327a37
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-05-19 23:04:15 +0430

    Add MyEnumerable and MyList

---
+++ MyList.rb
@@ -0,0 +1,13 @@
+require('./MyEnumerable')
+
+class MyList
+    include MyEnumerable
+
+    def initialize(*list)
+        @list = list
+    end
+
+    def each               
+        @list.each { |item| yield(item) if block_given? }
+    end
+end
